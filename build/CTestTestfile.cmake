bd833cec09b38f8a47e8ed552b7a4e93f2034996
PapaBear4 <bear.sidell@gmail.com>
2021-01-28 19:27:11 -0500

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/sidell/learnROS/src
+# Build directory: /home/sidell/learnROS/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("beginner_tutorials")
+subdirs("robot_setup_tf")
+subdirs("learning_tf2")
---
+++ build/beginner_tutorials/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/sidell/learnROS/devel/include".split(';') if "/home/sidell/learnROS/devel/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "beginner_tutorials"
+PROJECT_SPACE_DIR = "/home/sidell/learnROS/devel"
+PROJECT_VERSION = "0.0.0"
